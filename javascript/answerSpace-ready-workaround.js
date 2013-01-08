2fed39117b9296c2f9901170dec47d23742ff321
Ron Waldon <jokeyrhyme@gmail.com>
2013-01-08 12:36:53 +1100

    remove polling in favour of available event

--- javascript/answerSpace-ready-workaround.js
+++ javascript/answerSpace-ready-workaround.js
@@ -1,21 +1,12 @@
 /**
- * This should only be used until "viewReady" is fixed so that it triggers for the initial screen.
- * Any solution (like this one) that uses polling is a bad idea and not a long-term solution.
- *
+ * Available since 2012W35.
+ * 
  * This JavaScript could be inserted in a script tag in the htmlHead setting.
+ * Our 'viewShow' event is triggered after every view transition.
+ * Using jQuery's one() method has this code execute exactly once (just the first 'viewShow')..
  */
 
-$(document).ready(function() {
-  var boxedElements = $('.view > .box').children().length,
-      checkReady = function() {
-        if ($('#startUp').length === 0 && $('.view > .box').children().length !== boxedElements) {
-          // startup progress-dot screen is gone now
-          // TODO: fill this out with the code you want to run
-        } else {
-          // not gone yet, check again in 197 milliseconds
-          setTimeout(checkReady, 197);
-        }
-      };
-
-  checkReady();
+$(document).one('viewShow', function() {
+  // startup progress-dot screen is gone now
+  // TODO: fill this out with the code you want to run
 });
