06d5c041e68cff02e08f74d637b1dfe3920d9746
Ron Waldon <jokeyrhyme@gmail.com>
2013-01-14 17:00:24 +1100

    add subForm... events as per Mantis 1016

--- javascript/answerSpace-events.js
+++ javascript/answerSpace-events.js
@@ -1,28 +1,58 @@
 /* available since 2012W35 */
 
+
 $(document).on('viewShow', function() {
 // this = the .view element
-/* insert code to run after a view transition is completed */
+/* TODO: insert code to run after a view transition is completed */
 });
 
 $(document).on('viewReady', function() {
 // this = the .view element
-/* insert code to run after a view has been enhanced with Stars, Forms, Maps, etc */
+/* TODO: insert code to run after a view has been enhanced with Stars, Forms, Maps, etc */
 });
 
 $(document).on('formReady', function() {
 // this = the form element
-/* insert code to run after a form has been rendered, populated, and inserted into the page */
+/* TODO: insert code to run after a form has been rendered, populated, and inserted into the page */
 });
 
-/* for more details on form events, see: javascript/forms-submission-events.js */
 
-$(document).on('formSubmitSuccess', function() {
+$(document).on('formSubmitSuccess', function(event) {
 // this: not explicitly declared for this event
-/* insert code to run after a successful submission */
+/* now you can make use of the following attributes:
+ * - event.Blink.interaction
+ * - event.Blink.form
+ * - event.Blink.action
+ * - event.Blink.uuid
+ * - event.Blink.id (not available if action === 'add')
+ * - event.Blink.jqxhr (see: http://api.jquery.com/jQuery.ajax/#jqXHR)
+ */
+ /* TODO:insert code to run after a successful submission */
 });
 
-$(document).on('formSubmitError', function() {
+$(document).on('formSubmitError', function(event) {
 // this: not explicitly declared for this event
-/* insert code to run after a failed submission */
+/* see formSubmitSuccess example above */
+/* TODO: insert code to run after a failed submission */
+});
+
+/**
+ * Hint: use $(document).one(...) if you only want your code to run exactly one time.
+ */
+
+/**
+ * subForm... events available in Weekly 2013.03
+ */
+
+$(document).on('subFormReady', function(event) {
+// this = the subform element
+// event.Blink.formElement = the BlinkFormElement for the linking field
+// event.Blink.formObject = the BlinkFormObject for the sub record that was just added
+// TODO: insert code to run after a sub-form has been added or populated
+});
+
+$(document).on('subFormRemoved', function(event) {
+// this = the subform element
+// event.Blink.formElement = the BlinkFormElement for the linking field
+// TODO: insert code to run after a sub-form has been added or populated
 });
